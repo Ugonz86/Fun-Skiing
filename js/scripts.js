d174cba51c13140edcc1a708a71ec535ff842c2a
Ugonz86 <ugonzalez86@gmail.com>
2019-02-10 18:09:33 -0800

    add fun-skiing folder

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#initially-showing").fadeToggle();
+    $("#initially-hidden").fadeToggle();
+  });
+});
+
+$(function() {
+  $(".clickable2").click(function() {
+    $("#initially-showing2").fadeToggle();
+    $("#initially-hidden2").fadeToggle();
+  });
+});
+
+$(function() {
+  $(".clickable3").click(function() {
+    $("#initially-showing3").fadeToggle();
+    $("#initially-hidden3").fadeToggle();
+  });
+});
+
+$(function() {
+  $(".clickable4").click(function() {
+    $("#initially-showing4").fadeToggle();
+    $("#initially-hidden4").fadeToggle();
+  });
+});
